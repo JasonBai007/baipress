6c7c46429eb8ffa3393346f75281b6daee80ec3e
JasonBai007 <byygkcg@163.com>
2019-01-08 16:22:06 +0800

    完善第三个页面

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,7 +14,7 @@ module.exports = {
       { text: "VuePress", link: "https://vuepress.vuejs.org/zh/" },
       { text: "Github", link: "https://github.com/JasonBai007" }
     ],
-    sidebar: [["/coding/", "不以规矩，不能成方圆"], ["/tools/", "君子性非异也，善假于物也"], ["/philosophy/", "色即是空，空即是色"]],
+    sidebar: [["/coding/", "不以规矩，不能成方圆"], ["/tools/", "君子性非异也，善假于物也"], ["/philosophy/", "技术不是一切"]],
     lastUpdated: 'Last Updated',
   }
 };
